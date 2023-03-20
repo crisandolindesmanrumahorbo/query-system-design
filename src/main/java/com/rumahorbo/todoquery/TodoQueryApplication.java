d531463f13a01a87f4fa1fbb9a85ba436c283d12
Crisandolin Rumahorbo <crisandolin007@gmail.com>
2023-03-20 21:34:49 +0700

    first commit

---
+++ src/main/java/com/rumahorbo/todoquery/TodoQueryApplication.java
@@ -0,0 +1,13 @@
+package com.rumahorbo.todoquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rumahorbo/todoquery/TodoQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumahorbo.todoquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
